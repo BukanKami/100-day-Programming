4e2996fb5da889aa3ba7cbac315f25924473ec42
Bukan Saya <bukankami2021@gmail.com>
2024-08-29 12:00:16 +0800

    Duplicate Encoder (6 kyu) 54b42f9314d9229fd6000d9c

---
+++ JS/Day 001.js
@@ -0,0 +1,21 @@
+function duplicateEncode(word){
+    let data = {};
+    for (let index = 0; index < word.length; index++) {
+        if(data[word[index].toLowerCase()]!==undefined){
+            data[word[index].toLowerCase()] += 1;
+        }else{
+            data[word[index].toLowerCase()] = 1;
+        }
+    }
+    var returnValue ='';
+    
+    for (let index = 0; index < word.length; index++) {
+        if(data[word[index].toLowerCase()]===1){
+            returnValue += '(';
+        } else {
+            returnValue += ')';
+        }
+    }
+
+    return returnValue;
+}
