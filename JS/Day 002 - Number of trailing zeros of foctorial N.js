97439c5bcc32efaac580d920e9f2a9177b39e949
Bukan Saya <bukankami2021@gmail.com>
2024-08-30 13:39:18 +0800

    Number of trailing zeros of N! ( 5 Kyu ) 52f787eb172a8b4ae1000a34

---
+++ JS/Day 002 - Number of trailing zeros of foctorial N.js
@@ -0,0 +1,11 @@
+function zeros (n) {
+    if (n == 0) return 0
+    if (n < 9) return 1
+    let result = 0;
+    for (let i = 5; i <= n; i *= 5) {
+        result += Math.floor(n / i);
+    }
+    return result;
+}
+
+console.log(zeros(20))
