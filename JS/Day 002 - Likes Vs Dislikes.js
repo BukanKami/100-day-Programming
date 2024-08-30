b3d5b7ce96cb991d36cf0d51b28819a7457ab208
Bukan Saya <bukankami2021@gmail.com>
2024-08-30 18:30:36 +0800

    Likes Vs Dislikes ( 7 Kyu ) 62ad72443809a4006998218a

---
+++ JS/Day 002 - Likes Vs Dislikes.js
@@ -0,0 +1,14 @@
+// Like, Dislike, Nothing come from Preloaded
+
+function likeOrDislike(buttons) {
+    let returnData = 'Nothing';
+
+    return returnData;
+}
+
+console.log(likeOrDislike(['Like']));
+console.log(likeOrDislike(['Dislike','Like']));
+console.log(likeOrDislike(['Like','Dislike']));
+console.log(likeOrDislike(['Like','Like']));
+console.log(likeOrDislike(['Dislike','Dislike']));
+console.log(likeOrDislike(['Like','Dislike','Dislike']));
