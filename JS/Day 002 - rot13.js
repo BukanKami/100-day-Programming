e902617dbb9943367ecbc685ab283ea65658bf9c
Bukan Saya <bukankami2021@gmail.com>
2024-08-30 13:38:41 +0800

    look up dan regex

--- JS/Day 002 - rot13.js
+++ JS/Day 002 - rot13.js
@@ -24,5 +24,27 @@ function rot13(message){
 
     return result
 }
+// 800 > ms
+
+// function rot13(message) {
+//     const lookup = {
+//         a: 'n', b: 'o', c: 'p', d: 'q', e: 'r', f: 's', g: 't', h: 'u', i: 'v', j: 'w', k: 'x', l: 'y', m: 'z',
+//         n: 'a', o: 'b', p: 'c', q: 'd', r: 'e', s: 'f', t: 'g', u: 'h', v: 'i', w: 'j', x: 'k', y: 'l', z: 'm',
+//         A: 'N', B: 'O', C: 'P', D: 'Q', E: 'R', F: 'S', G: 'T', H: 'U', I: 'V', J: 'W', K: 'X', L: 'Y', M: 'Z',
+//         N: 'A', O: 'B', P: 'C', Q: 'D', R: 'E', S: 'F', T: 'G', U: 'H', V: 'I', W: 'J', X: 'K', Y: 'L', Z: 'M'
+//     };
+
+//     return Array.from(message).map(char => lookup[char] || char).join('');
+// }
+// 700 > ms, somtime 600
+
+let message = "Test";
+const resrot13 = message => 
+    message.replace(/[a-zA-Z]/g, char => {
+        const base = char <= 'Z' ? 65 : 97;
+        return String.fromCharCode(((char.charCodeAt(0) - base + 13) % 26) + base);
+    });
+
+console.log(resrot13(message));  
 
 console.log(rot13("Test")); // Should return "Grfg"
