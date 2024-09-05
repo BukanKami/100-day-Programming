c673a4a8955f218b721ad2670ba49681e6c83b69
Bukan Saya <bukankami2021@gmail.com>
2024-09-05 15:38:48 +0800

    Perhitungan pada variable

--- JS/Day 003 - Human Readable Time.js
+++ JS/Day 003 - Human Readable Time.js
@@ -1,18 +1,25 @@
-function humanReadable (seconds) {
-    if(seconds <= 0) return "00:00:00";
-    if(seconds == 359999) return "99:59:59";
+// function humanReadable (seconds) {
+//     if(seconds <= 0) return "00:00:00";
+//     if(seconds == 359999) return "99:59:59";
 
-    var H = Math.floor(seconds / 3600);
-    if(H > 0){
-        seconds = seconds - (3600 * H);
-    }
+//     var H = Math.floor(seconds / 3600);
+//     if(H > 0){
+//         seconds = seconds - (3600 * H);
+//     }
 
-    var M = Math.floor(seconds / 60);
-    if(M > 0){
-        seconds = seconds - (60 * M);
-    }
+//     var M = Math.floor(seconds / 60);
+//     if(M > 0){
+//         seconds = seconds - (60 * M);
+//     }
 
-    return (H < 10 ? '0'+H : H) + ':' + (M < 10 ? '0'+M :M) + ':' + (seconds < 10 ? '0'+seconds :seconds);
+//     return (H < 10 ? '0'+H : H) + ':' + (M < 10 ? '0'+M :M) + ':' + (seconds < 10 ? '0'+seconds :seconds);
+// }
+
+function humanReadable(sec) {
+    let h = sec / 3600 | 0,
+        m = (sec - h * 3600) / 60 | 0,
+        s = sec - h * 3600 - m * 60;
+    return `${h < 10 ? '0' + h : h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`;
 }
 
 console.log(humanReadable(359999));
