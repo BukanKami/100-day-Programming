cd4675b6b3ace7571f01277031590571fc54598b
Bukan Saya <bukankami2021@gmail.com>
2024-09-05 09:52:42 +0800

    menggunakan map

--- JS/Day 003 - Stop gninnipS My sdroW.js
+++ JS/Day 003 - Stop gninnipS My sdroW.js
@@ -1,21 +1,27 @@
-function spinWords(string){
-    //TODO Have fun :)
-    let returnString = '';
-    let words = string.split(' ');
-    for(let i = 0; i < words.length; i++){
-        if (i > 0 && words.length > 1 && i < words.length){
-            returnString += ' ';
-        }
+// function spinWords(string){
+//     //TODO Have fun :)
+//     let returnString = '';
+//     let words = string.split(' ');
+//     for(let i = 0; i < words.length; i++){
+//         if (i > 0 && words.length > 1 && i < words.length){
+//             returnString += ' ';
+//         }
 
-        if(words[i].length >= 5){
-            let reversedWord = words[i].split('').reverse().join('');
-            returnString = returnString + reversedWord;
-        }else{
-            returnString = returnString + words[i];
-        }
-    }
+//         if(words[i].length >= 5){
+//             let reversedWord = words[i].split('').reverse().join('');
+//             returnString = returnString + reversedWord;
+//         }else{
+//             returnString = returnString + words[i];
+//         }
+//     }
 
-    return returnString;
+//     return returnString;
+// }
+
+function spinWords(words){
+    return words.split(" ").map(function(word){
+        return word.length <= 5 ? word : word.split('').reverse().join(''); 
+    });
 }
 
 console.log(spinWords('Welcome'));
