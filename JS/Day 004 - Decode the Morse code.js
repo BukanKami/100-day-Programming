6f2a82f32553c7385afb8b510c9f4eb76d128b30
Bukan Saya <bukankami2021@gmail.com>
2024-09-16 13:55:51 +0800

    Decode the Morse code ( 6 Kyu ) 54b724efac3d5402db00065e

---
+++ JS/Day 004 - Decode the Morse code.js
@@ -0,0 +1,51 @@
+let morseCode = '.... . -.--   .--- ..- -.. .';
+
+let code = {
+    "...---...": "SOS",
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    "-.-.--": "!",
+    ".-.-.-": ".",
+};
+
+let result = "";
+const words = morseCode.trim().split("  ");
+
+for (let i = 0; i < words.length; i++) {
+    const letters = words[i].split(" ");
+    for (let j = 0; j < letters.length; j++) {
+        if (code[letters[j]] !== undefined) {
+        result += code[letters[j]];
+        }
+    }
+
+    if (i < words.length - 1) {
+        result += " ";
+    }
+}
+
+console.log(result)
