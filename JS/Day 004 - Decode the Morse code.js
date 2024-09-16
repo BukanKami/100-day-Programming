118619a83c16fc1728730b318b301de8511464c9
Bukan Saya <bukankami2021@gmail.com>
2024-09-16 13:57:35 +0800

    using  MORSE_CODE[letter]

--- JS/Day 004 - Decode the Morse code.js
+++ JS/Day 004 - Decode the Morse code.js
@@ -49,3 +49,14 @@ for (let i = 0; i < words.length; i++) {
 }
 
 console.log(result)
+
+// using  MORSE_CODE[letter]
+decodeMorse = function (morseCode) {
+    function decodeMorseLetter(letter) {
+        return MORSE_CODE[letter];
+    }
+    function decodeMorseWord(word) {
+        return word.split(" ").map(decodeMorseLetter).join("");
+    }
+    return morseCode.trim().split("   ").map(decodeMorseWord).join(" ");
+};
