7a75cdd283ec7ed2234a855582359798ef029c1e
Bukan Saya <bukankami2021@gmail.com>
2024-09-17 09:31:24 +0800

    one line using map

--- JS/Day 005 - Reversed sequence.js
+++ JS/Day 005 - Reversed sequence.js
@@ -1,10 +1,15 @@
+// const reverseSeq = n => {
+//     var returnArray = [];
+//     while(n>0){
+//         returnArray.push(n);
+//         n--;
+//     }
+//     return returnArray;
+// };
+
 const reverseSeq = n => {
-    var returnArray = [];
-    while(n>0){
-        returnArray.push(n);
-        n--;
-    }
-    return returnArray;
+    return Array(n).fill(0).map((e, i) => n - i );
 };
 
 console.log(reverseSeq(5)); // Output: [5, 4, 3, 2, 1]
+
